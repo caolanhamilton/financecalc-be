05f2af6e6abff96c97746103d1dc83120c305cac
Caolan <caolanhamilton75@gmail.com>
2023-06-28 21:34:53 +0100

    add handling for user already existing

--- app.ts
+++ app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import { postUser } from "./controllers/users.controllers";
 import {
   postApplication,
@@ -6,14 +7,13 @@ import {
 } from "./controllers/applications.controllers";
 import { authenticate, authenticationError } from "aws-cognito-express";
 import { checkCognitoObjectExists } from "./custommiddleware/checkCognitoObjectExists";
-import cors from "cors";
+
 import { authConfig } from "./authConfig";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.options("/applications", cors());
 
 app.use(authenticate(authConfig));
 
--- models/user.models.ts
+++ models/user.models.ts
@@ -3,7 +3,16 @@ import { User } from "../types/types";
 
 export async function addUser(sub: string, user: User) {
   try {
-    const res = await dbPool.query(
+    const existingUserCheck = await dbPool.query(
+      "SELECT * FROM users WHERE uid = $1",
+      [sub]
+    );
+
+    if (existingUserCheck.rows.length > 0) {
+      return;
+    }
+
+    const insertUserQuery = await dbPool.query(
       "INSERT INTO users(uid, first_name, second_name, email) VALUES($1, $2, $3, $4)",
       [sub, user.first_name, user.second_name, user.email]
     );
