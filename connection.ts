a0129d9ee2fce3d78ad1b5d578ccdb43b0f23f91
Caolan <caolanhamilton75@gmail.com>
2023-06-28 14:42:36 +0100

    update connection method

--- connection.ts
+++ connection.ts
@@ -1,18 +1,12 @@
 import { Pool, PoolConfig } from "pg";
 import dotenv from "dotenv";
 dotenv.config();
-
 const config: PoolConfig = {
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
 };
-
 const dbPool = new Pool(config);
 
 export default dbPool;
